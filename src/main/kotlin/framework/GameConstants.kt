b210726b060189697cd080a200e943275de38299
Frederik Jacobsen <Frederik1904@gmail.com>
2020-11-03 09:57:41 +0100

    Moved types and Effectlevels into GameConstants, got intellij to autogenerate equals method for Position, removed the tests for position as it is no longer needed

--- src/main/kotlin/framework/GameConstants.kt
+++ src/main/kotlin/framework/GameConstants.kt
@@ -4,3 +4,18 @@ object GameConstants {
     val HashCodePrimeValue: Int = 337213
 
 }
+
+enum class Types(val type: String) {
+    BUG("Bug"), DARK("Dark"), DRAGON("Dragon"), ELECTRIC("Electric"), FAIRY("Fairy"),
+    FIGHTING("Fighting"), FIRE("Fire"), FLYING("Flying"), GHOST("Ghost"), GRASS("Grass"),
+    GROUND("Ground"), ICE("Ice"), NORMAL("Normal"), POISON("Poison"), PSYCHIC("Psychic"),
+    ROCK("Rock"), STEEL("Steel"), WATER("Water");
+
+    override fun toString(): String {
+        return type
+    }
+}
+
+enum class EffectLevel(val multiplier: Double) {
+    Inefective(0.0), Weak(0.5), Normal(1.0), Strong(2.0);
+}
--- src/main/kotlin/framework/Position.kt
+++ src/main/kotlin/framework/Position.kt
@@ -1,22 +1,23 @@
 package framework
 
 class Position(var row: Int, var col: Int) {
-    override fun equals(other: Any?): Boolean {
-        if (other == null
-            || other !is Position
-            || other.col != col
-            || other.row != row
-        ) {
-            return false
-        }
-        return true
-    }
-
     override fun hashCode(): Int {
         return GameConstants.HashCodePrimeValue * row + col;
     }
 
     override fun toString(): String {
-        return String.format("[$row,$col]")
+        return "[$row,$col]"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Position
+
+        if (row != other.row) return false
+        if (col != other.col) return false
+
+        return true
     }
 }
